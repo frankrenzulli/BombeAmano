71891da32ee503f9eeca04e7df3310c1cc02727e
frankrenzulli <frankrenzulli00@gmail.com>
2023-04-10 00:26:46 +0200

    AnimationFix

--- BombeAmano/Assets/Scripts/P1Movement.cs
+++ BombeAmano/Assets/Scripts/P1Movement.cs
@@ -8,20 +8,14 @@ public class P1Movement : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 direction = Vector2.down;
     public float speed = 5f;
-
-    public SpriteRenderer UpSprite;
-    public SpriteRenderer DownSprite;
-    public SpriteRenderer LeftSprite;
-    public SpriteRenderer RightSprite;
+    
 
     public Animator anim;
 
-    private SpriteRenderer playerSprite;
     private void Awake()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -29,22 +23,24 @@ public class P1Movement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
+            
             SetDirection(Vector2.up);
             anim.SetBool("DownWalk", false);
             anim.SetBool("UpWalk", true);
             anim.SetBool("LeftWalk", false);
             anim.SetBool("RightWalk", false);
-            playerSprite.sprite = UpSprite.sprite;
+
 
         }
         else if (Input.GetKey(KeyCode.S))
         {
+            
             SetDirection(Vector2.down);
             anim.SetBool("DownWalk", true);
             anim.SetBool("UpWalk", false);
             anim.SetBool("LeftWalk", false);
             anim.SetBool("RightWalk", false);
-            playerSprite.sprite = DownSprite.sprite;
+
 
         }
         else if (Input.GetKey(KeyCode.A))
@@ -55,22 +51,28 @@ public class P1Movement : MonoBehaviour
             anim.SetBool("UpWalk", false);
             anim.SetBool("LeftWalk", true);
             anim.SetBool("RightWalk", false);
-            playerSprite.sprite = LeftSprite.sprite;
+
 
         }
         else if (Input.GetKey(KeyCode.D))
         {
-
+            
             SetDirection(Vector2.right);
             anim.SetBool("DownWalk", false);
             anim.SetBool("UpWalk", false);
             anim.SetBool("LeftWalk", false);
             anim.SetBool("RightWalk", true);
-            playerSprite.sprite = RightSprite.sprite;
+
         }
         else
         {
+
+            anim.SetBool("DownWalk", false);
+            anim.SetBool("UpWalk", false);
+            anim.SetBool("LeftWalk", false);
+            anim.SetBool("RightWalk", false);
             SetDirection(Vector2.zero);
+            
 
         }
 
